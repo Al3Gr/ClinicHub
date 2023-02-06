0db6ed217c7193e09aac340da87478090e519004
Elena Volpe <elenavolpe001@gmail.com>
2023-02-06 18:50:33 +0100

    aggiunta classe Exam e ExamType

---
+++ progetto/src/main/java/org/example/domain/Exam.java
@@ -0,0 +1,35 @@
+package org.example.domain;
+
+import java.util.Calendar;
+import java.sql.Time;
+import java.util.Random;
+
+public class Exam {
+    private float price;
+    private int code;
+    private Calendar bookingDate;
+    private Calendar readyDate;
+    private Time time;
+    private Patient patient;
+    private Doctor doctor;
+    private ExamType type;
+
+    public float getPrice() {return price;};
+    public int getCode() {return code;};
+    public Calendar getBookingDate() {return bookingDate;};
+
+    public Calendar getReadyDate() { return readyDate;};
+    public Time getTime() {return time;};
+    public void setPatient(Patient p) {patient=p;};
+    public void setDoctor(Doctor d) {doctor=d;};
+    public Exam(ExamType type){
+        this.type=type;
+        code= new Random().nextInt();
+        price=type.getPrice();
+    }
+    public void setData(Calendar data) {
+        bookingDate=data;
+        readyDate=data;
+        readyDate.add(Calendar.DAY_OF_MONTH, type.getDaysToReady());
+    }
+}
---
+++ progetto/src/main/java/org/example/domain/ExamType.java
@@ -0,0 +1,24 @@
+package org.example.domain;
+public enum ExamType {
+
+    BLOOD_ANALYSIS(30,3),
+    URINE_ANALYSIS(20,2),
+    MAMMOGRAPHY(40,7),
+    ECHOCARDIOGRAPHY(50,8),
+    ELECTROMYOGRAPHY(50,8),
+    ELECTROENCEPHALOGRAPHY(50,7);
+    private float price;
+    private int daysToReady; //voi avete messo Time, io ho messo questo
+    ExamType(float price, int daysToReady) {
+        this.price = price;
+        this.daysToReady = daysToReady;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public int getDaysToReady() {
+        return daysToReady;
+    }
+}
