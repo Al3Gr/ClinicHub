7f4232e2452e0d680968b7af30485c7648704848
Elena Volpe <elenavolpe001@gmail.com>
2023-02-07 14:50:35 +0100

    aggiunta classe Utility

---
+++ progetto/src/main/java/org/example/domain/Utility.java
@@ -0,0 +1,14 @@
+package org.example.domain;
+import java.util.Date;
+import java.util.List;
+import java.sql.Time;
+public class Utility {
+    public void loadPatient(){}
+    public void loadDoctor(){}
+    public List<Date> getDates(){
+        return null;
+    }
+    public List<Time> getTimes(){
+        return null;
+    }
+}
