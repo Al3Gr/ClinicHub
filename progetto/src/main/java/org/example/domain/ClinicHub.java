bafd09610032bb58d3d917995bb0c6129ee994b9
Alessandro Giuseppe Gravagno <alessandro.gravagno@hotmail.com>
2023-02-14 09:31:22 +0100

    .

--- progetto/src/main/java/org/example/domain/ClinicHub.java
+++ progetto/src/main/java/org/example/domain/ClinicHub.java
@@ -246,7 +246,9 @@ public class ClinicHub {
         if (currentExam != null) {
             return EmailService.sendResult(currentExam, currentExam.getResultInfo());
         } else {
+            System.out.println("d");
             throw new Exception("Ordine di chiamata dei metodi non rispettato");
+
         }
     }
 
